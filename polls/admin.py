dae66a00467238cf633bb413a6345e489cce6eef
Dhruvil Dave <dhruvil@gkmit.co>
2020-04-13 17:40:33 +0530

    first django api done with admin view

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['publish_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question_text', 'publish_date', 'was_published_recently')
+    list_filter = ['publish_date']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
